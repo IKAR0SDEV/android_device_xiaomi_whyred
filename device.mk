101ef413c9fff67162b60eb3e40d5ed18ae511e9
GuaiYiHu <guaiyihu@foxmail.com>
2019-09-13 10:47:10 +0800

    [DNM]whyred: Disable WfdCommon
    Change-Id: I9cbb7e6756d7e2e789f83e1435eac712c40f0572

--- device.mk
+++ device.mk
@@ -441,8 +441,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
-# Wi-Fi Display
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 export EXTENDED_BUILD_TYPE=OFFICIAL
